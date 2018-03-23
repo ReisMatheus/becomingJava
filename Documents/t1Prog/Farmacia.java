02ebe3d89dc5e6a9fc3d0d1b4939ba0817d943e3
matheusReis <Matheusmlcr@gmail.com>
2018-03-23 11:28:51 -0300

    Cadastro fora do switch()

--- Documents/t1Prog/Farmacia.java
+++ Documents/t1Prog/Farmacia.java
@@ -6,40 +6,41 @@ public class Farmacia {
 		System.out.println("Cadastro e compra de medicamentos");
 		int sair = 0;
 		while(sair==0){
+			int i=1, cadastro = 0;
+			while(cadastro == 0){
+				System.out.println("Digite o nome do medicamento:\n");
+				String nome = entrada.nextLine();
+				System.out.println("Digite o preco do medicamento:\n");
+				Double precoVenda = entrada.nextDouble();
+				System.out.println("Digite a data de validade do medicamento:\n");
+				String dataValidade = entrada.nextLine();
+				System.out.println("Digite o laboratoria do medicamento:\n");
+				String laboratorio = entrada.nextLine();
+				while(true){
+					System.out.println("O medicamento necessita de receita?:\n(1) Sim\n(0) Nao");
+					Boolean receitaObrigatoria = entrada.nextBoolean();
+					if((receitaObrigatoria==true) || (receitaObrigatoria==false)){
+						break;
+					}
+				System.out.println("Digite (1) para sair\nDigite (0) para Sair");
+				cadastro = entrada.nextInt();
+				}
+				System.out.println("Digite a quantidade do medicamento no estoque:\n");
+				int qtdEstoque = entrada.nextInt();
+				Remedios popular = new Remedios(nome, precoVenda, dataValidade, laboratorio, receitaObrigatoria, qtdEstoque);
+			}
 			int opcao = entrada.nextInt();
-			System.out.println("Para cadastrar digite (1)\nPara reajustar preco de venda digite (2)\nPara vender digite (3)\nPara comprar digite (4)\n");
+			System.out.println("\nPara reajustar preco de venda digite (2)\nPara vender digite (3)\nPara comprar digite (4)\n");
 			switch(opcao){
 				case 1:{
-					int i=1;
-					System.out.println("Digite o nome do medicamento:\n");
-					String nome = entrada.nextLine();
-					System.out.println("Digite o preco do medicamento:\n");
-					Double precoVenda = entrada.nextDouble();
-					System.out.println("Digite a data de validade do medicamento:\n");
-					String dataValidade = entrada.nextLine();
-					System.out.println("Digite o laboratoria do medicamento:\n");
-					String laboratorio = entrada.nextLine();
-					while(true){
-						System.out.println("O medicamento necessita de receita?:\n(1) Sim\n(0) Nao");
-						Boolean receitaObrigatoria = entrada.nextBoolean();
-						if((receitaObrigatoria==true) || (receitaObrigatoria==false)){
-							break;
-						}
-					}
-					System.out.println("Digite a quantidade do medicamento no estoque:\n");
-					int qtdEstoque = entrada.nextInt();
-					Remedios popular = new Remedios(
-													nome, precoVenda, dataValidade,
-													 laboratorio, receitaObrigatoria, 
-													 qtdEstoque);
-					i++;
 					break;
 				}
 				case 2:{
+					System.out.println("Para reajustar");
 					break;
 				}
 				case 3:{
-					if(this.i>=1){
+					if(int i>=1){
 						System.out.println("Qual medicamento você deseja?\n");
 						int quantidade = entrada.nextInt();
 						popular.vender(quantidade);
